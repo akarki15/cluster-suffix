21357b6dbad26d11782a5d4ebae7909a670c0a84
akarki15 <akarki15@amherst.edu>
2014-12-03 23:26:02 -0500

    Output folder now gets the output.

--- createsubmits.sh
+++ createsubmits.sh
@@ -6,7 +6,7 @@ while [ $counter  -le $upLimit ]; do
 		echo "UNIVERSE = vanilla
 EXECUTABLE = /home/akarki15/suffix/run.jar
 ERROR = /home/akarki15/suffix/error/$counter.txt
-OUTPUT = /home/akarki15/scp/output/$book1"_"$book2"_"$counter.txt
+OUTPUT = /home/akarki15/suffix/output/$book1"_"$book2"_"$counter.txt
 ARGUMENTS = /home/akarki15/suffix/input/$book1.txt /home/akarki15/suffix/input/$book2.txt 12
 INITIALDR = /home/akarki15/suffix/output/
 NOTIFICATION = never
