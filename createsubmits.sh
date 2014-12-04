404df0069e4d4d103fa9c8b8bb5973d452017251
akarki15 <akarki15@amherst.edu>
2014-12-04 00:19:48 -0500

    Seriously why are outputs blank?

--- createsubmits.sh
+++ createsubmits.sh
@@ -7,8 +7,6 @@ while [ $counter  -le $upLimit ]; do
 EXECUTABLE = /home/akarki15/suffix/run.jar
 ERROR = /home/akarki15/suffix/error/$counter.txt
 OUTPUT = /home/akarki15/suffix/output/$book1"_"$book2"_"$counter.txt
-ARGUMENTS = /home/akarki15/suffix/input/$book1.txt /home/akarki15/suffix/input/$book2.txt 12
-INITIALDR = /home/akarki15/suffix/output/
 NOTIFICATION = never
 QUEUE" > submit-files/$book1"_"$book2"_"$counter.submit-file
 		condor_submit submit-files/$book1"_"$book2"_"$counter.submit-file
